a07e1f66b21a5a9894eb00603f3c9d44ddd3133d
Daizu <daizutabi@gmail.com>
2024-08-25 23:33:07 +0000

    __all__

--- src/hydraflow/__init__.py
+++ src/hydraflow/__init__.py
@@ -1,6 +1,9 @@
 from .context import Info, chdir_artifact, log_run, watch
 from .mlflow import set_experiment
 from .runs import (
+    Run,
+    Runs,
+    drop_unique_params,
     filter_runs,
     get_artifact_dir,
     get_artifact_path,
@@ -9,19 +12,24 @@ from .runs import (
     get_param_names,
     get_run,
     get_run_id,
+    load_config,
 )
 
 __all__ = [
     "Info",
+    "Run",
+    "Runs",
     "chdir_artifact",
+    "drop_unique_params",
     "filter_runs",
     "get_artifact_dir",
     "get_artifact_path",
     "get_artifact_uri",
-    "get_run",
     "get_param_dict",
     "get_param_names",
+    "get_run",
     "get_run_id",
+    "load_config",
     "log_run",
     "set_experiment",
     "watch",
