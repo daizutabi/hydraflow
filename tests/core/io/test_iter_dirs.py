e992a9b8be5787c9088ddb1d3e4db5c1e5e5f875
Daizu <daizutabi@gmail.com>
2025-03-17 12:01:21 +0000

    Refactor test functions to include root_dir parameter
    - Updated test functions in `test_iter_dirs.py` to accept `root_dir` as a parameter for improved directory handling.
    - Adjusted calls to `iter_experiment_dirs`, `iter_run_dirs`, `iter_artifacts_dirs`, and `iter_artifact_paths` to use the new parameter structure.
    - Enhanced test coverage for filtering experiment directories using parameterized tests.

--- tests/core/io/test_iter_dirs.py
+++ tests/core/io/test_iter_dirs.py
@@ -35,24 +35,35 @@ def test_root_dir(root_dir: Path):
     assert get_root_dir() == root_dir
 
 
-def test_iter_experiment_dirs():
+def test_iter_experiment_dirs(root_dir: Path):
     from hydraflow.core.io import get_experiment_name, iter_experiment_dirs
 
-    names = [get_experiment_name(p) for p in iter_experiment_dirs()]
+    names = [get_experiment_name(p) for p in iter_experiment_dirs(root_dir)]
     assert sorted(names) == ["e1", "e2"]  # type: ignore
 
 
-def test_iter_experiment_dirs_filter():
+@pytest.mark.parametrize(
+    ("e", "es"),
+    [("e1", ["e1"]), ("e*", ["e1", "e2"]), ("*", ["e1", "e2"]), ("*2", ["e2"])],
+)
+def test_iter_experiment_dirs_glob(root_dir: Path, e, es):
     from hydraflow.core.io import get_experiment_name, iter_experiment_dirs
 
-    it = iter_experiment_dirs(experiment_names="e1")
+    names = [get_experiment_name(p) for p in iter_experiment_dirs(root_dir, e)]
+    assert sorted(names) == es  # type: ignore
+
+
+def test_iter_experiment_dirs_filter(root_dir: Path):
+    from hydraflow.core.io import get_experiment_name, iter_experiment_dirs
+
+    it = iter_experiment_dirs(root_dir, experiment_names="e1")
     assert [get_experiment_name(p) for p in it] == ["e1"]
 
 
-def test_iter_experiment_dirs_filter_callable():
+def test_iter_experiment_dirs_filter_callable(root_dir: Path):
     from hydraflow.core.io import get_experiment_name, iter_experiment_dirs
 
-    it = iter_experiment_dirs(lambda name: name == "e2")
+    it = iter_experiment_dirs(root_dir, experiment_names=lambda name: name == "e2")
     assert [get_experiment_name(p) for p in it] == ["e2"]
 
 
@@ -75,21 +86,21 @@ def test_get_experiment_name_metafile_none(root_dir: Path):
     assert get_experiment_name(root_dir) is None
 
 
-def test_iter_run_dirs():
+def test_iter_run_dirs(root_dir: Path):
     from hydraflow.core.io import iter_run_dirs
 
-    assert len(list(iter_run_dirs())) == 5
+    assert len(list(iter_run_dirs(root_dir))) == 5
 
 
-def test_iter_artifacts_dirs():
+def test_iter_artifacts_dirs(root_dir: Path):
     from hydraflow.core.io import iter_artifacts_dirs
 
-    assert len(list(iter_artifacts_dirs())) == 5
+    assert len(list(iter_artifacts_dirs(root_dir))) == 5
 
 
-def test_iter_artifact_paths():
+def test_iter_artifact_paths(root_dir: Path):
     from hydraflow.core.io import iter_artifact_paths
 
-    it = iter_artifact_paths("text.txt")
+    it = iter_artifact_paths(root_dir, "text.txt")
     text = sorted("".join(p.read_text() for p in it))
     assert text == ["1", "2", "3", "4", "5"]
