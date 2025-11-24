38489b275a8a30535c30400cdf7f6548139bde5d
Daizu <daizutabi@gmail.com>
2025-11-24 15:10:05 +0900

    fix: Refactor test_log to use temporary directory and copy update.py (#241)

--- tests/core/main/test_dry_run.py
+++ tests/core/main/test_dry_run.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import shutil
 import subprocess
 import sys
 from pathlib import Path
@@ -25,9 +26,11 @@ def test_len(results: Results):
     assert len(results) == 1
 
 
-def test_log():
-    file = Path(__file__).parent.joinpath("update.py").as_posix()
-    args = [sys.executable, file, "-m", "width=2,3", "height=4,5", "--dry-run"]
+def test_log(tmp_path: Path, monkeypatch: pytest.MonkeyPatch):
+    monkeypatch.chdir(tmp_path)
+    file = Path(__file__).parent.joinpath("update.py")
+    shutil.copy(file, "update.py")
+    args = [sys.executable, "update.py", "-m", "width=2,3", "height=4,5", "--dry-run"]
     out = subprocess.check_output(args, text=True)
     assert "width: 2\nheight: 4\narea: 8" in out
     assert "width: 2\nheight: 5\narea: 10" in out
